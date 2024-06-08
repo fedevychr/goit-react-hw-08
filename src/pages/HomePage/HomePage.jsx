957d5ed8d11d415eec5669de4ee480824d6c6ad2
Roman Fedevych <fedevychr@gmail.com>
2024-06-08 18:05:40 +0300

    fin home, not found

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
+import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
+
+import css from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <DocumentTitle>Home</DocumentTitle>
+
+      <div className={css.container}>
+        <h1 className={css.title}>
+          WELCOME TO PHONEBOOK APP
+          <span role="img" aria-label="Greeting icon">
+            📗
+          </span>
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <h1>Page you visited doesn&apos;t exist</h1>
+      <Link to="/">Go Home</Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
