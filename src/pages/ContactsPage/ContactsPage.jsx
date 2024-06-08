648fa1f4c4f373c5621a2e8491dd8c525b2388bf
Roman Fedevych <fedevychr@gmail.com>
2024-06-08 18:19:08 +0300

    fin contacts and app

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,5 +1,45 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
+import {
+  selectPhoneBookIsError,
+  selectPhoneBookIsLoading,
+} from "../../redux/contacts/selectors";
+import Loader from "../../components/Loader/Loader";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import AddContactForm from "../../components/AddContactForm/AddContactForm";
+import Contact from "../../components/Contact/Contact";
+import { selectFilteredContacts } from "../../redux/filters/selectors";
+import SearchBox from "../../components/SearchBox/SearchBox";
+
+import css from "./ContactsPage.module.css";
+
 const ContactsPage = () => {
-  return <div>ContactsPage</div>;
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectFilteredContacts);
+  const isLoading = useSelector(selectPhoneBookIsLoading);
+  const isError = useSelector(selectPhoneBookIsError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  return (
+    <div className={css.contactContainer}>
+      <AddContactForm />
+      <SearchBox />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      <ul className={css.listContact}>
+        {Array.isArray(contacts) && contacts.length === 0 && (
+          <li>You don&apos;t have any added contacts yet!</li>
+        )}
+        {Array.isArray(contacts) &&
+          contacts.map((contact) => {
+            return <Contact key={contact.id} contact={contact} />;
+          })}
+      </ul>
+    </div>
+  );
 };
 
 export default ContactsPage;
